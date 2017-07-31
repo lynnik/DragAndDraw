5bc3c2c05eff13afebee9d18177a96c4d5cf10f5
Lynnik Vlad <lynnik.v@gmail.com>
2017-07-31 09:00:28 +0300

    Project created.

---
+++ app/src/main/java/com/example/lynnik/draganddraw/BoxDrawingView.java
@@ -0,0 +1,17 @@
+package com.example.lynnik.draganddraw;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class BoxDrawingView extends View {
+
+  public BoxDrawingView(Context context) {
+    super(context);
+  }
+
+  public BoxDrawingView(Context context, @Nullable AttributeSet attrs) {
+    super(context, attrs);
+  }
+}
