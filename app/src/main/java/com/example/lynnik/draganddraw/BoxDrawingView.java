217bff592f89653d7f9151b43c8438f9591bd94d
Lynnik Vlad <lynnik.v@gmail.com>
2017-07-31 09:11:45 +0300

    Added handling of custom touches.

--- app/src/main/java/com/example/lynnik/draganddraw/BoxDrawingView.java
+++ app/src/main/java/com/example/lynnik/draganddraw/BoxDrawingView.java
@@ -1,11 +1,16 @@
 package com.example.lynnik.draganddraw;
 
 import android.content.Context;
+import android.graphics.PointF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class BoxDrawingView extends View {
+
+  public static final String TAG = "BoxDrawingView";
 
   public BoxDrawingView(Context context) {
     super(context);
@@ -13,5 +18,30 @@ public class BoxDrawingView extends View {
 
   public BoxDrawingView(Context context, @Nullable AttributeSet attrs) {
     super(context, attrs);
+  }
+
+  @Override
+  public boolean onTouchEvent(MotionEvent event) {
+    PointF current = new PointF(event.getX(), event.getY());
+    String action = "";
+
+    switch (event.getAction()) {
+      case MotionEvent.ACTION_DOWN:
+        action = "ACTION_DOWN";
+        break;
+      case MotionEvent.ACTION_MOVE:
+        action = "ACTION_MOVE";
+        break;
+      case MotionEvent.ACTION_UP:
+        action = "ACTION_UP";
+        break;
+      case MotionEvent.ACTION_CANCEL:
+        action = "ACTION_CANCEL";
+        break;
+    }
+
+    Log.i(TAG, action + " at x=" + current.x + ", y=" + current.y);
+
+    return true;
   }
 }
