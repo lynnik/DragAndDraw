e2d9ed710d7bc5c4ad0e16906adb1ca42e15889d
Lynnik Vlad <lynnik.v@gmail.com>
2017-07-31 09:33:00 +0300

    Added drawing rectangles.

--- app/src/main/java/com/example/lynnik/draganddraw/BoxDrawingView.java
+++ app/src/main/java/com/example/lynnik/draganddraw/BoxDrawingView.java
@@ -1,6 +1,8 @@
 package com.example.lynnik.draganddraw;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.graphics.PointF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
@@ -17,6 +19,8 @@ public class BoxDrawingView extends View {
 
   private Box mCurrentBox;
   private List<Box> mBoxen = new ArrayList<>();
+  private Paint mBoxPaint;
+  private Paint mBackgroundPaint;
 
   public BoxDrawingView(Context context) {
     super(context);
@@ -24,6 +28,12 @@ public class BoxDrawingView extends View {
 
   public BoxDrawingView(Context context, @Nullable AttributeSet attrs) {
     super(context, attrs);
+
+    mBoxPaint = new Paint();
+    mBoxPaint.setColor(0x22ff0000);
+
+    mBackgroundPaint = new Paint();
+    mBackgroundPaint.setColor(0xfff8efe0);
   }
 
   @Override
@@ -57,5 +67,18 @@ public class BoxDrawingView extends View {
     Log.i(TAG, action + " at x=" + current.x + ", y=" + current.y);
 
     return true;
+  }
+
+  @Override
+  protected void onDraw(Canvas canvas) {
+    canvas.drawPaint(mBackgroundPaint);
+
+    for (Box box : mBoxen) {
+      float left = Math.min(box.getOrigin().x, box.getCurrent().x);
+      float right = Math.max(box.getOrigin().x, box.getCurrent().x);
+      float top = Math.min(box.getOrigin().y, box.getCurrent().y);
+      float bottom = Math.max(box.getOrigin().y, box.getCurrent().y);
+      canvas.drawRect(left, top, right, bottom, mBoxPaint);
+    }
   }
 }
